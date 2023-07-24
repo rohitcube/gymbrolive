e1f9c988d896b01c64e45743b0cf22760a8428f1
rohit <rohitdarnell@gmail.com>
2023-07-24 18:54:21 +0100

    second commit

--- app/controllers/feedback.controller.js
+++ app/controllers/feedback.controller.js
@@ -1,37 +1,69 @@
 const Feedback = require('../models/feedback.model');
 
-/*xports.createFeedback = async (req, res) => {
-    if (!req.body.email || !req.body.paragraph)  {
+exports.createFeedback = async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.paragraph) {
         res.status(400).send({
           message: "Email and message are required."
         });
         return;
       }
-};
-*/
-exports.createFeedback = async (req, res) => {
-    try {
-        if (!req.body.email || !req.body.paragraph)  {
-            res.status(400).send({
-              message: "Email and message are required."
-            });
-        }
   
-      // Check if there is a pre-existing booking at the specified date and start time
-   /*   const existingBooking = await Booking.findOne({
-        where: {
-          date: date,
-          start_time: start_time
-        }
-      });
-  
-      if (existingBooking) {
-        return res.status(409).json({ message: 'Booking already exists at this time and date' });
-      }
-  */
       const newFeedback = await Feedback.create(req.body);
       res.status(201).json({ message: 'Feedback sent successfully', feedback: newFeedback });
     } catch (error) {
       res.status(400).json({ error: 'Feedback could not be sent' });
     }
   };
+  
+  exports.getAllFeedback = async (req, res) => {
+    try {
+      const feedback = await Feedback.findAll();
+      res.status(200).json(feedback);
+    } catch (error) {
+      res.status(400).json({ error: 'Error while fetching feedback' });
+    }
+  };
+
+  
+  exports.getFeedbackById = async (req, res) => {
+    const { feedbackId } = req.params;
+    try {
+      const feedback = await Feedback.findByPk(feedbackId);
+      if (!feedback) {
+        return res.status(404).json({ message: 'Feedback not found' });
+      }
+      res.status(200).json(feedback);
+    } catch (error) {
+      res.status(400).json({ error: 'Error while fetching feedback' });
+    }
+  };
+  
+  exports.updateFeedback = async (req, res) => {
+    const { feedbackId } = req.params;
+    try {
+      const feedback = await Feedback.findByPk(feedbackId);
+      if (!feedback) {
+        return res.status(404).json({ message: 'Feedback not found' });
+      }
+      await feedback.update(req.body);
+      res.status(200).json({ message: 'Feedback updated successfully', feedback });
+    } catch (error) {
+      res.status(400).json({ error: 'Error while updating feedback' });
+    }
+  };
+  
+  exports.deleteFeedback = async (req, res) => {
+    const { feedbackId } = req.params;
+    try {
+      const feedback = await Feedback.findByPk(feedbackId);
+      if (!feedback) {
+        return res.status(404).json({ message: 'Feedback not found' });
+      }
+      await feedback.destroy();
+      res.status(200).json({ message: 'Feedback deleted successfully' });
+    } catch (error) {
+      res.status(400).json({ error: 'Error while deleting feedback' });
+    }
+  };
+  
