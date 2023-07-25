ff345236427a450787b183c53cb3c309a5aa305c
rohitcube <104619746+rohitcube@users.noreply.github.com>
2023-07-25 18:09:31 +0100

    Update tele.js

--- app/tele/tele.js
+++ app/tele/tele.js
@@ -2,7 +2,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 // Replace 'YOUR_TELEGRAM_BOT_API_TOKEN' with your actual Telegram Bot API token
-const bot = new TelegramBot(secrets.bot_token_no, { polling: true });
+const bot = new TelegramBot(secrets.BOT_TOKEN_NO, { polling: true });
 
 console.log('file runs');
 
